cde601b6ddae192c9918917bc6dcde0b22b0573d
Ryan Chapkis <rchapkis@ucsc.edu>
2020-10-19 15:18:33 -0700

    trying to figure out the bidirectional search;

--- CMPM-146-Assignment-2-main/P__export/src/p2_pathfinder_a_star_copy.py
+++ CMPM-146-Assignment-2-main/P__export/src/p2_pathfinder_a_star_copy.py
@@ -1,3 +1,4 @@
+"""
 import heapq
 import math
 
@@ -11,55 +12,11 @@ def find_path (source_point, destination_point, mesh):
     #Mesh is a dictionary with box dimensions and lists of adjacent boxes
     #Source and dest are POINTS! Not boxes!
 
-    """
-    Searches for a path from source_point to destination_point through the mesh
-    Args:
-        source_point: starting point of the pathfinder
-        destination_point: the ultimate goal the pathfinder must reach
-        mesh: pathway constraints the path adheres to
-    Returns:
-        A path (list of points) from source_point to destination_point if exists
-        A list of boxes explored by the algorithm
-    """
-
     #First thing we want to try is returning a list of boxes
-
-    """
-    Pseudocode
-    Scan through the boxes in mesh to find source_box and dest_box
-    for each box in mesh:
-        if source_point(x) is between top and bottom right x
-            if source_point(y) is between top and bottom right y
-                source_box = this box
-    create a priority queue, push source_point
-    create a dictionary came_from containing previous locations
-    while priority queue is not empty:
-        current_box = queue.pop
-        if current_box = destination
-            create list path_taken
-            using came_from, starting at the desintation, 
-                add boxes to path_taken
-            return path_taken
-        
-        for each neighbor of current_box (using mesh)
-            (There are no distances so we don't have to worry about that rn)
-            if neighbor is not in came_from:
-                came_from.append(neighbor)
-                queue.push(neighbor)
-    
-    (did not find a path)
-    return None
-    """
 
     source_box = (0, 0, 0, 0)
     dest_box = (0, 0, 0, 0)
     detail_points = {} #a dictionary the maps boxes to (x,y) pairs.
-
-    """
-    How to do point to point:
-    - start w/ source point in first box
-    - take current point and constrain it within the bounds of next box
-    """
 
     #Find the boxes that source and destination are in
     for box in mesh['boxes']:
@@ -74,28 +31,20 @@ def find_path (source_point, destination_point, mesh):
     #The keys for both parts of the mesh are quadruples
 
     path = []
+    #previous boxes that have been traversed
+    boxes = {}
     path_taken = []
 
     #the distance traversed thus far
     distance_so_far = {}
     distance_so_far[source_point] = 0
 
-    detail_points[source_box] = source_point
-
     start_heuristic = euclidean_dist(source_point, destination_point)
+
     frontier = [(start_heuristic, source_box, source_point)]
 
-    #previous boxes that have been traversed
-    boxes = {}
     boxes[source_box] = None
-
-    #dictionary that stores forward backpointers
-    forward_prev = {}
-    forward_prev[source_box] = None
-
-    #dictionary that stores backward backpointers
-    backward_prev = {}
-    backward_prev[dest_box] = None
+    detail_points[source_box] = source_point
 
     while(len(frontier) > 0):
         priority, current_box, current_point = heapq.heappop(frontier)
@@ -103,7 +52,7 @@ def find_path (source_point, destination_point, mesh):
         if current_box == dest_box:
             # Insert current_box into boxes, w/ previous as value
             path.append(destination_point)
-            while current_box is not None:
+            while(current_box != None):
                 path_taken.append(current_box)
                 path.append(detail_points[current_box])
                 current_box = boxes[current_box] #destination point should already have something in boxes
@@ -113,14 +62,6 @@ def find_path (source_point, destination_point, mesh):
         for neighbor in neighbors:
             if(neighbor not in boxes):
 
-                """
-                Take current point and constrain it within the range of the current neighbors
-                    rangeX = currentBox(x1 - x2) * neighborbox(x1 - x2)
-                    rangeY = currentBox(y1 - y2) * neighborBox(y1 - y2)
-                    neighborPoint = current_point
-                    constrain(neighborPoint.x, rangeX)
-                    constrain(neighborPoint.y, rangeY)
-                """
                 xMin, yMin = max(current_box[0], neighbor[0]), max(current_box[2], neighbor[2])
                 xMax, yMax = min(current_box[1], neighbor[1]), min(current_box[3], neighbor[3])
                              
@@ -140,3 +81,5 @@ def find_path (source_point, destination_point, mesh):
                     heapq.heappush(frontier, (priority, neighbor, neighbor_point))
 
     return path, path_taken #Replaced boxes.keys() w/ path_taken
+
+    """
